df93aa57f7557c5c6cb73b5affddd471b22fb4f0
atiehmerikh <at.merikh@gmail.com>
2021-05-23 12:29:57 +0200

    polynomial coefficients of DRebA method for Neck

--- dREBA/main.py
+++ dREBA/main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import poly_coeff_calculator as pCoeff
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    m_pcoeff = pCoeff.polynomial_generator()
+    m_pcoeff.neck_polynomial([0,10,20,80],[0,2,1,2])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ dREBA/poly_coeff_calculator.py
@@ -0,0 +1,36 @@
+import sympy as sym
+import numpy as np
+
+
+
+class polynomial_generator:
+
+    def squared_error(self, real_value,predicted_value):
+        error = 0
+        for i in range(len(real_value)):
+            error += (predicted_value[i] - real_value[i])**2
+        error = sym.sympify(error)
+        return 1/(len(real_value))*error
+
+    def neck_polynomial(self,training_points,real_partial_REBA_values):
+        a_2 = sym.Symbol('a_2')
+        a_1 = sym.Symbol('a_1')
+        a_0 = sym.Symbol('a_0')
+
+        polynomial =[]
+        for i in range(0,len(training_points)):
+            polynomial.append(a_2*training_points[i]**2+a_1*training_points[i]+a_0)
+
+        sq_error = sym.sympify(sym.expand(self.squared_error(real_partial_REBA_values,polynomial)))
+        df_da2 = sym.diff(sq_error,a_2)
+        df_da1 = sym.diff(sq_error,a_1)
+        df_da0 = sym.diff(sq_error,a_0)
+
+        a = np.array([[df_da2.args[1].args[0], df_da2.args[2].args[0], df_da2.args[3].args[0]],
+                      [df_da1.args[1].args[0], df_da1.args[2].args[0], df_da1.args[3].args[0]],
+                      [df_da0.args[1].args[0], df_da0.args[2].args[0], df_da0.args[3].args[0]]
+                      ],dtype='float')
+        b = np.array([df_da2.args[0], df_da1.args[0], df_da0.args[0]],dtype='float')
+        x = np.linalg.solve(a, b)
+        print(x)
+
