4bedea7c162d213b8a93d4f31ae3f34a56cd8083
Omid Gheibi <omid.gheibi@gmail.com>
2021-05-30 15:34:30 +0200

    train dREBA based on random samples.

--- dREBA/main.py
+++ dREBA/main.py
@@ -1,9 +1,27 @@
 import numpy as np
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+import REBA.body_part_reba_calculator.Degree_to_REBA.neck_reba_score as REBA_neck
+import REBA.body_part_reba_calculator.Degree_to_REBA.trunk_reba_score as REBA_trunk
+import REBA.body_part_reba_calculator.Degree_to_REBA.leg_reba_score as REBA_leg
+import REBA.body_part_reba_calculator.Degree_to_REBA.upperarm_reba_score as REBA_UA
+import REBA.body_part_reba_calculator.Degree_to_REBA.lowerarm_reba_score as REBA_LA
+import REBA.body_part_reba_calculator.Degree_to_REBA.wrist_reba_score as REBA_wrist
+import REBA.body_part_reba_calculator.partial_REBA_to_total_REBA as REBA
 
 import polynomial_coeff_calculator as pCoeff
 import Dreba_coeff_calculator as DREBA
 import sympy as sym
+import random
 
+def calc_total_reba(x):
+    return REBA.partial_to_total_REBA([REBA_neck.NeckREBA(list(x[0:3])).neck_reba_score(),\
+                                       REBA_trunk.TrunkREBA(list(x[3:6])).trunk_reba_score(),\
+                                       REBA_leg.LegREBA([x[6],x[6]]).leg_reba_score(), \
+                                       REBA_UA.UAREBA(list(x[7:13])).upper_arm_reba_score(),\
+                                       REBA_LA.LAREBA(list(x[13:15])).lower_arm_score(),\
+                                       REBA_wrist.WristREBA(list(x[15:])).wrist_reba_score()]).find_total_REBA()
 
 def dReba_coeff_generator(M,N,A):
     # M is a n to m matrix,
@@ -105,16 +123,41 @@ def dREBA_polynomial_matrix_generator():
 
     return A
 
+def train_dREBA(sample_size):
+    random.seed(1)
+    qss = [[-60,0,20], [-54,0, 54], [-60,0, 60],\
+          [-30,0,20,60], [-40,0, 40], [-35,0, 35],\
+          [0,30,60],\
+          [-20,0,20,45], [-20, 0, 20, 45], [-2,0], [-2,0], [0, 30], [0, 30],\
+          [0, 60, 100], [0, 60, 100],\
+          [-53,-15,15], [-53,-15,15], [-40,0, 30], [-40,0, 30], [-90,0, 90], [-90,0, 90]]
+
+
+    samples = np.zeros(shape=(sample_size, 21))
+    samples_REBA = np.zeros(shape=(sample_size,))
+    for i in range(sample_size):      
+        a_sample = np.zeros(shape=(21,))
+        for j, qs in enumerate(qss):
+            a_sample[j] = random.sample(qs,1)[0]
+        samples[i,:] = a_sample
+        samples_REBA[i] = calc_total_reba(a_sample)
+
+    generator = dREBA_polynomial_matrix_generator()
+    dREBA_coeffs = dReba_coeff_generator(samples,samples_REBA,generator)
+    return dREBA_coeffs
+
 
 if __name__ == '__main__':
+    train_dREBA(10000)
    # reading the input file(M,N)
-   A = dREBA_polynomial_matrix_generator()
-   dREBA_coeffs = dReba_coeff_generator(M,N,A)
+#    A = dREBA_polynomial_matrix_generator()
 
-   each_posture_error=[]
-   n = len(N)
-   for i in range(n):
-       m_dREBA = DREBA.Dreba_error_each_posture()
-       each_posture_error.append(m_dREBA.dReba_error(A,M[i,:],N[i],dREBA_coeffs))
+#    dREBA_coeffs = dReba_coeff_generator(M,N,A)
 
-    print(each_posture_error)
+#    each_posture_error=[]
+#    n = len(N)
+#    for i in range(n):
+#        m_dREBA = DREBA.Dreba_error_each_posture()
+#        each_posture_error.append(m_dREBA.dReba_error(A,M[i,:],N[i],dREBA_coeffs))
+
+#     print(each_posture_error)
