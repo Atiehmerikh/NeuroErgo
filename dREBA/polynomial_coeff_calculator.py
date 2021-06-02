ae0e0209117bae1a1379a69aa70f87ffa199425c
Omid Gheibi <omid.gheibi@gmail.com>
2021-06-02 18:55:47 +0200

    the problem of coefficients is solved

--- dREBA/polynomial_coeff_calculator.py
+++ dREBA/polynomial_coeff_calculator.py
@@ -11,6 +11,39 @@ class polynomial_generator:
         error = sym.sympify(error)
         return 1 / (len(real_value)) * error
 
+    def __extract_a_polynomial_coefficients(self, polynomial):
+        a_2 = sym.Symbol('a_2')
+        a_1 = sym.Symbol('a_1')
+        a_0 = sym.Symbol('a_0')
+        coeffs = [0.0, 0.0, 0.0, 0.0]
+        for a in polynomial.args:
+            if len(a.args) == 2:
+                if a.args[1] == a_0:
+                    coeffs[1] = a.args[0]
+                elif a.args[1] == a_1:
+                    coeffs[2] = a.args[0]
+                elif a.args[1] == a_2:
+                    coeffs[3] = a.args[0]
+            # elif len(a.args) == 1:
+            #     if a.args[1] == a_0:
+            #         coeffs[1] = 1
+            #     elif a.args[1] == a_1:
+            #         coeffs[2] = 1
+            #     elif a.args[1] == a_2:
+            #         coeffs[3] = 1
+            elif len(a.args) == 0:
+                if a == a_0:
+                    coeffs[1] = 1
+                elif a == a_1:
+                    coeffs[2] = 1
+                elif a == a_2:
+                    coeffs[3] = 1
+                else:
+                    coeffs[0] = a
+            else:
+                raise Exception("Only for polynomials up to degree 2")
+        return coeffs
+
     def polynomial_coefficients_calculator(self,training_points,real_partial_REBA_values):
         a_2 = sym.Symbol('a_2')
         a_1 = sym.Symbol('a_1')
@@ -25,11 +58,18 @@ class polynomial_generator:
         df_da1 = sym.diff(sq_error,a_1)
         df_da0 = sym.diff(sq_error,a_0)
 
-        a = np.array([[df_da2.args[1].args[0], df_da2.args[2].args[0], df_da2.args[3].args[0]],
-                      [df_da1.args[1].args[0], df_da1.args[2].args[0], df_da1.args[3].args[0]],
-                      [df_da0.args[1].args[0], df_da0.args[2].args[0], df_da0.args[3].args[0]]
-                      ],dtype='float')
-        b = np.array([df_da2.args[0], df_da1.args[0], df_da0.args[0]],dtype='float')
+        print()
+        df_da2_coeffs = self.__extract_a_polynomial_coefficients(df_da2)
+        df_da1_coeffs = self.__extract_a_polynomial_coefficients(df_da1)
+        df_da0_coeffs = self.__extract_a_polynomial_coefficients(df_da0)
+        #a = [[0.0,0.0,0.0], [0.0,0.0,0.0], [0.0,0.0,0.0]]
+
+        print(df_da2_coeffs)
+        print(df_da1_coeffs)
+        print(df_da0_coeffs)
+
+        a = np.array([df_da2_coeffs[1:4], df_da1_coeffs[1:4], df_da0_coeffs[1:4]], dtype='float')
+        b = np.array([df_da2_coeffs[0], df_da1_coeffs[0], df_da0_coeffs[0]], dtype='float')
         # [a_2,a_1,a_0]
         coeffs = np.linalg.solve(a, b)
         return coeffs
