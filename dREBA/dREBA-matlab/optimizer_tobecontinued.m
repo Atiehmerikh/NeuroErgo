8e479af154b7d0a827647861b8182038927bf80b
Omid Gheibi <omid.gheibi@gmail.com>
2021-08-26 18:02:47 +0200

    refactor optimizer and foldering corresponding data

--- dREBA/dREBA-matlab/optimizer_tobecontinued.m
+++ dREBA/dREBA-matlab/optimizer_tobecontinued.m
@@ -2,8 +2,8 @@
 
 % M is body degrees, a 21 x 21 matrix
 % N is total reba scores for each row of M, a 21 x 1 matrix
-M = csvread('data/M.csv');
-N = csvread('data/N.csv');
+M = csvread('data/input/M.csv');
+N = csvread('data/input/N.csv');
 
 A=poly_coeff_calculator();
 display('A is found');
@@ -13,8 +13,8 @@ ws = diffrentiable_reba_coeff_calculator(M, N,A);
 
 %% test dREBA based on test data analogous to M and N, generated in Python
 
-M_test = csvread('data/M_test.csv');
-N_test = csvread('data/N_test.csv');
+M_test = csvread('data/input/M_test.csv');
+N_test = csvread('data/input/N_test.csv');
 
 
 Q = M_test.^2 .* (A(:,1).') + M_test .* (A(:,2).') + (A(:,3).');
@@ -25,7 +25,7 @@ estimate(estimate > 15) = 15;
 estimate(estimate < 1) = 1;
 
 % write test results
-csvwrite('dREBA_estimate.csv', estimate);
+csvwrite('data/output/dREBA_estimate.csv', estimate);
 
 ep = abs(estimate - N_test);
-csvwrite('error_data.csv', ep);
+csvwrite('data/output/error_data.csv', ep);
